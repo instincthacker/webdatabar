ea15475736eb3638c45a79154299a0326489fb4d
npcaron <accessoriez0@gmail.com>
2018-04-17 16:02:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>DataBar</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by FreeHTML5.co" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FreeHTML5.co" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'> -->
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	
+	
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="header-inner">
+				<a href="index.html"><img src="images/logo.png"></a>
+				<nav role="navigation">
+					<ul>
+						<li><a href="products.html">Products</a></li>
+						<li><a href="services.html">Services</a></li>
+						<!-- <li><a href="pricing.html">Pricing</a></li> -->
+						<li><a href="partners.html">Partner</a></li>
+						<li><a href="contact.html">Contact</a></li>
+						<!-- <li class="cta"><a href="#">Get started</a></li> -->
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	
+
+	<aside id="fh5co-hero" class="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(images/slides/GTA_1.png);">
+<!-- 		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="col-md-12 col-md-offset-1 text-right js-fullheight slider-text ">
+		   				<div class="slider-text-inner">
+		   					<h2>Start Your Startup With This Template</h2>
+		   					<p><a href="#" class="btn btn-primary btn-lg">Get started</a></p>
+		   				</div>
+		   			</div>
+		   		</div> -->
+		   	</li>
+		   	<li style="background-image: url(images/slides/GTA_2.png);">
+		   		<!-- <div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner">
+		   					<h2>Take Your Business To The Next Level</h2>
+		   					<p><a href="#" class="btn btn-primary btn-lg">Get started</a></p>
+		   				</div>
+		   			</div>
+		   		</div> -->
+		   	</li>
+		   	<li style="background-image: url(images/slides/GTA_4.png);">
+		   		<!-- <div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner">
+		   					<h2>We Think Different That Others Can't</h2>
+		   					<p><a href="#" class="btn btn-primary btn-lg">Get started</a></p>
+		   				</div>
+		   			</div>
+		   		</div> -->
+		   	</li>
+		  	</ul>
+	  	</div>
+	</aside>
+
+	<div id="fh5co-why-us" class="animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Why Choose Us</h2>
+					<p>อธิบายเกี่ยวกับเราอย่างคร่าว </p>
+				</div>
+				<div class="col-md-4 col-xs-12 text-center item-block">
+					<span class="icon"><img src="images/30.svg" alt="Free HTML5 Templates" class="img-responsive"></span>
+					<h3>Service</h3>
+					<p>อธิบายอย่างคร่าวๆ</p>
+					<p><a href="#" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 col-xs-12 text-center item-block">
+					<span class="icon"><img src="images/18.svg" alt="Free HTML5 Templates" class="img-responsive"></span>
+					<h3>Product</h3>
+					<p>อธิบายอย่างคร่าวๆ</p>
+					<p><a href="#" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 col-xs-12 text-center item-block">
+					<span class="icon"><img src="images/27.svg" alt="Free HTML5 Templates" class="img-responsive"></span>
+					<h3>Partners</h3>
+					<p>อธิบายอย่างคร่าวๆ</p>
+					<p><a href="#" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+	<div class="fh5co-section-with-image">
+		
+		<img src="images/image_1.jpg" alt="" class="img-responsive">
+		<div class="fh5co-box animate-box">
+			<h2>จุดดึงดูดความสนใจ</h2>
+			<p>ใส่ content ที่หน้าสนใจลงไปสักอย่าง</p>
+			<p><a href="#" class="btn btn-primary btn-outline with-arrow">Get started <i class="icon-arrow-right"></i></a></p>
+		</div>
+		
+	</div>
+
+	<div id="fh5co-grid-products" class="animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Our Products</h2>
+					<p>We have 4 group of products. </p>
+				</div>
+			</div>
+		</div>
+
+
+	<div class="row" style="padding: 5%;">
+		<div class="col-md-3" class="animate-box">
+			<a href="#" class="item-grid one" style="background-image:url(images/image_4.jpg);">
+				<div class="v-align">
+					<div class="v-align-middle">
+						<h3 class="title">สินค้ากลุ่มที่ 1</h3>
+						<h5 class="category">รายละเอียดคร่าวๆ</h5>
+					</div>
+				</div>
+			</a>
+		</div>
+		<div class="col-md-3" class="animate-box">
+			<a href="#" class="item-grid one" style="background-image:url(images/image_2.jpg);">
+				<div class="v-align">
+					<div class="v-align-middle">
+						<h3 class="title">สินค้ากลุ่มที่ 2</h3>
+						<h5 class="category">รายละเอียดคร่าวๆ</h5>
+					</div>
+				</div>
+			</a>
+		</div>
+		<div class="col-md-3" class="animate-box">
+			<a href="#" class="item-grid one" style="background-image:url(images/image_3.jpg);">
+				<div class="v-align">
+					<div class="v-align-middle">
+						<h3 class="title">สินค้ากลุ่มที่ 3</h3>
+						<h5 class="category">รายละเอียดคร่าวๆ</h5>
+					</div>
+				</div>
+			</a>
+		</div>	
+		<div class="col-md-3" class="animate-box">
+
+			<a href="#" class="item-grid one" style="background-image:url(images/image_1.jpg);">
+				<div class="v-align">
+					<div class="v-align-middle">
+						<h3 class="title">สินค้ากลุ่มที่ 4</h3>
+						<h5 class="category">รายละเอียดคร่าวๆ</h5>
+					</div>
+				</div>
+			</a>
+		</div>		
+	</div>
+
+	
+</div>		
+	
+
+	<div id="fh5co-blog" class="animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Our Customer</h2>
+					<p>We're trust from many companies in Thiland.</p>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+				<div class="row">
+					<?php 
+					$c = '
+					<div class="col-md-3 col-xs-6">
+						<img src="images/image_1.jpg" class="img-responsive" style="padding: 20%;">
+					</div>';
+						for ($o=0; $o < 12; $o++) { 
+							# code...
+							echo $c;
+						}
+					?>
+
+				</div>
+				</div>
+			</div>
+				<div class="col-md-12 text-center">
+					<br>
+					<p><a href="#" class="btn btn-primary btn-outline with-arrow">View More <i class="icon-arrow-right"></i></a></p>
+				</div>
+		</div>	
+	</div>
+	<footer id="fh5co-footer" role="contentinfo">
+	
+		<div class="container">
+			<div class="col-md-5 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>About Us</h3>
+				DATABAR Company Limited (Branch 0001)<br>
+				1448/15 Crystal Design Center (CDC), <br>
+				L2 2FL. Room 202. 204. 206.208. <br>
+				Soi Ladprao 87 (Chandra Suk) <br>
+				Praditmanuthum Rd. Klongjan Bangkapi Bangkok 10240 <br>
+				<div class="icon-phone"> +66 2 102 2591	</div>
+				<div class="icon-envelope"> sales@databar.co.th </div>
+				<!-- <p><a href="#" class="btn btn-primary btn-outline with-arrow btn-sm">I'm button <i class="icon-arrow-right"></i></a></p> -->
+			</div>
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Follow Us</h3>
+				<ul class="fh5co-social">
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-google-plus"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+				</ul>
+			</div>
+			
+			
+			<div class="col-md-12 fh5co-copyright text-center">
+				<p>&copy; 2018 DataBar Co.,Ltd . All Rights Reserved.</p>	
+			</div>
+			
+		</div>
+	</footer>
+	</div>
+	
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
